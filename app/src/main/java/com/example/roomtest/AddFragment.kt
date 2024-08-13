cfe68ea7f37664e303dfab74da056b850173e268
jiangrenxiang <j1197434376@163.com>
2024-08-13 10:56:00 +0800

    1.将键盘聚焦的english输入上

--- app/src/main/java/com/example/roomtest/AddFragment.kt
+++ app/src/main/java/com/example/roomtest/AddFragment.kt
@@ -1,25 +1,31 @@
 package com.example.roomtest
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.FragmentActivity
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AddFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class AddFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var activity1: FragmentActivity
+    lateinit var button: Button
+    lateinit var english: EditText
+    lateinit var chinese: EditText
+    lateinit var inputMethodManager: InputMethodManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,23 +43,21 @@ class AddFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_add, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+        activity1 = requireActivity()
+        button = activity1.findViewById(R.id.button)
+        english = activity1.findViewById(R.id.english)
+        chinese = activity1.findViewById(R.id.chinese)
+        button.isEnabled = false
+        english.requestFocus()
+        inputMethodManager = activity1.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(english,0)
+
+
     }
+
+
+
 }
