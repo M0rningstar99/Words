5ce8043f940fe433a04310a7617a9fc0235e01e9
jiangrenxiang <j1197434376@163.com>
2024-08-13 11:36:53 +0800

    1.完成了commit的功能

--- app/src/main/java/com/example/roomtest/AddFragment.kt
+++ app/src/main/java/com/example/roomtest/AddFragment.kt
@@ -2,6 +2,8 @@ package com.example.roomtest
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +12,8 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,6 +30,8 @@ class AddFragment : Fragment() {
     lateinit var english: EditText
     lateinit var chinese: EditText
     lateinit var inputMethodManager: InputMethodManager
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,6 +53,7 @@ class AddFragment : Fragment() {
 
         super.onViewCreated(view, savedInstanceState)
         activity1 = requireActivity()
+        var wordViewModel:WordViewModel = ViewModelProvider(activity1).get(WordViewModel::class.java)
         button = activity1.findViewById(R.id.button)
         english = activity1.findViewById(R.id.english)
         chinese = activity1.findViewById(R.id.chinese)
@@ -54,9 +61,38 @@ class AddFragment : Fragment() {
         english.requestFocus()
         inputMethodManager = activity1.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.showSoftInput(english,0)
+        var textWatcher = object:TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                var english = english.text
+                var chinese = chinese.text
+                button.isEnabled = !english.isEmpty() && !chinese.isEmpty()
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        }
+        english.addTextChangedListener(textWatcher)
+        chinese.addTextChangedListener(textWatcher)
+        button.setOnClickListener{
+            var english = english.getText().toString().trim()
+            var chinese = chinese.getText().toString().trim()
+            var word = Word(english,chinese)
+            wordViewModel.insertWords(word)
+            var navController = Navigation.findNavController(view)
+            navController.navigateUp()
+
+
+        }
 
     }
+
 
 
 
